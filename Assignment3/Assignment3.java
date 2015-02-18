664ad90775e0ecba2dfd23d1f86df4dda5fb34a3
Chris Jones <cjjones22@gmail.com>
2015-02-17 20:47:00 -0600

    Replaced school's KeyboardReader w/ Java Scanner.

--- Assignment3/Assignment3.java
+++ Assignment3/Assignment3.java
@@ -1,16 +1,18 @@
-import csci130.*;
+import java.util.Scanner;
 
 public class Assignment3{
 	public static void main(String[] args){
 
+    Scanner keyboard = new Scanner(System.in);
+
 		System.out.print("Enter the coefficient of the quadratic term: ");
-		double quadratic_co = KeyboardReader.readDouble();
+		double quadratic_co = keyboard.nextDouble();
 
 		System.out.print("Enter the coefficient of the linear term: ");
-		double linear_co = KeyboardReader.readDouble();
+		double linear_co = keyboard.nextDouble();
 
 		System.out.print("Enter the constant term: ");
-		double constant = KeyboardReader.readDouble();
+		double constant = keyboard.nextDouble();
 
 		Polynomial myPolynomial = new Polynomial();
 		myPolynomial.constructPolynomial(quadratic_co, linear_co, constant);
@@ -20,7 +22,7 @@ public class Assignment3{
 		System.out.println();
 
 		System.out.print("Enter the function argument: ");
-		double arg = KeyboardReader.readDouble();
+		double arg = keyboard.nextDouble();
 
 		myPolynomial.functionValue(arg);
 		System.out.println("f(" + arg + ") = " + myPolynomial.functionValue(arg));
